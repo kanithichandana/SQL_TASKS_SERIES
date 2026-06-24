a586a7ff6fce7aa36b20aa7e1d848dd9f9f15e71
kanithichandana <kanithichandana2004@gmail.com>
2026-06-24 21:15:40 +0530

    Add files via upload

---
+++ SQL_TASKS(SELECT,WHERE,ORDER BY).sql
@@ -0,0 +1,29 @@
+/*SELECT *
+FROM Customers;*/
+
+/*SELECT first_name,country
+FROM customers*/
+
+/*SELECT *
+FROM customers
+WHERE age>25*/
+
+/*SELECT first_name,last_name,country FROM customers
+WHERE country='USA'*/
+
+/*SELECT * FROM customers
+WHERE age BETWEEN 22 AND 28*/
+
+/*SELECT * FROM orders
+WHERE amount > 500*/
+
+/*SELECT * FROM orders
+ORDER BY amount DESC*/
+
+/*SELECT * FROM Shippings
+WHERE status='Pending'*/
+
+/*SELECT  DISTINCT country  FROM Customers*/
+
+/*SELECT * FROM Customers 
+WHERE first_name ='John'*/
